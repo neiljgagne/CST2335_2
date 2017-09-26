b9edad77be606f8fa5e5d185ac4fdd47aa74c6b0
neil <gagn0420@algonquinlive.com>
2017-09-26 15:52:49 -0400

    Lab 2 commit final

--- app/src/main/java/com/example/neilg_000/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/neilg_000/androidlabs/ListItemsActivity.java
@@ -1,9 +1,9 @@
 package com.example.neilg_000.androidlabs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
 
-public class ListItemsActivity extends AppCompatActivity {
+public class ListItemsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
